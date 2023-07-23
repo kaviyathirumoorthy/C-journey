c12cc4b68e4e8b581ea74f4f259f228a8d04290f
Kaviya. T <90082734+kaviyathirumoorthy@users.noreply.github.com>
2023-07-23 22:27:37 +0530

    Update expand the string.c

--- problems solved/dynamic programming camp train/expand the string.c
+++ problems solved/dynamic programming camp train/expand the string.c
@@ -6,11 +6,15 @@ for(int i=0;i<str[i]!='\0';i++){
 int k=i;
 int ans=0;
 while(str[i+1]>=48 && str[i+1]<=57){
-ans=ans*10+(str[i]-48);
+ans=ans*10+(str[i+1]-48);
 i++;
 }
 for(int i=1;i<=ans;i++) printf("%c",str[k]);
 }
 }
+
+// if input is a1b3c6 output is abbbcccccc
+//if input is q2w3e4 qqwwweeee
+
 
 
